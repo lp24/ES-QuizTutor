10689870982d4d8debcb3460ce507823c20ce11a
BasantaPoudel <basanta.poudel@tecnico.ulisboa.pt>
2021-03-22 10:34:26 +0000

    Add new classes

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/domain/Question.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/domain/Question.java
@@ -38,6 +38,7 @@ public class Question implements DomainEntity {
         public static final String MULTIPLE_CHOICE_QUESTION = "multiple_choice";
         public static final String CODE_FILL_IN_QUESTION = "code_fill_in";
         public static final String CODE_ORDER_QUESTION = "code_order";
+        public static final String MULTIPLE_CHOICE_ORDER_QUESTION = "multiple_choice_order";
     }
 
     @Id
