68b6a2ebc69f6290c740d2bd7a1723f79e242ca3
rcanunes <rodrigo.ca.nunes99@gmail.com>
2021-03-26 02:17:14 +0000

    [PCI 1.4, 1.5, 1.6] - Implement Test Cases - close #44, #45, #46

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/impexp/service/ImportExportItemCombinationQuestionsTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/impexp/service/ImportExportItemCombinationQuestionsTest.groovy
@@ -4,16 +4,16 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.TestConfiguration
 import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
 import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
-import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.ItemCombinationQuestion
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.ItemCombinationQuestionDto
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto
-import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.User
 
 @DataJpaTest
 class ImportExportItemCombinationQuestionsTest extends SpockTest{
+    def questionDto
+    
     def setup() {
-        def questionDto = new QuestionDto()
+        questionDto = new QuestionDto()
         questionDto.setTitle(QUESTION_1_TITLE)
         questionDto.setContent(QUESTION_1_CONTENT)
         questionDto.setStatus(Question.Status.AVAILABLE.name())
@@ -25,7 +25,7 @@ class ImportExportItemCombinationQuestionsTest extends SpockTest{
         def questionsXml = questionService.exportQuestionsToXml()
         print questionsXml
         and: 'a clean database'
-        questionService.removeQuestion(questionId)
+        questionService.removeQuestion(questionDto.getId())
 
         when:
         questionService.importQuestionsFromXml(questionsXml)
@@ -42,6 +42,7 @@ class ImportExportItemCombinationQuestionsTest extends SpockTest{
     def "export item combination questions to latex"() {
         when:
         def questionsLatex = questionService.exportQuestionsToLatex()
+        print questionsLatex
 
         then:
         questionsLatex != null
