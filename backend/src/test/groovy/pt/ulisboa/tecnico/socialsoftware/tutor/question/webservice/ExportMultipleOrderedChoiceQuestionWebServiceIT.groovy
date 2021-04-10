94984e3e5b8aa864acaa64211f9bbb17b15d032f
luana-marques <luana.marques@tecnico.ulisboa.pt>
2021-04-10 16:51:11 +0100

    Modified ExportMultipleOrderedChoiceQuestionWebServiceIT.groovy

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/webservice/ExportMultipleOrderedChoiceQuestionWebServiceIT.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/webservice/ExportMultipleOrderedChoiceQuestionWebServiceIT.groovy
@@ -1,12 +1,19 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.question.webservice
 
+import groovyx.net.http.HttpResponseException
 import groovyx.net.http.RESTClient
+import org.apache.http.HttpStatus
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.CustomExceptionHandler
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
+import java.nio.file.AccessDeniedException
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.ACCESS_DENIED
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.QUESTIONS_EXPORT_ERROR;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ExportMultipleOrderedChoiceQuestion extends SpockTest{
+class ExportMultipleOrderedChoiceQuestionWebServiceIT extends SpockTest{
     @LocalServerPort
     private int port
     def courseExecutionDto
@@ -30,11 +37,33 @@ class ExportMultipleOrderedChoiceQuestion extends SpockTest{
 
         when: "the web service is invoked"
         def map = restClient.get(
-                path: "/courses/" + courseExecutionDto.getCourseExecutionId() +"/questions/export",
+                path: "/courses/" + courseExecutionDto.getCourseExecutionId() + "/questions/export",
                 requestContentType: "application/zip"
         )
 
         then: "the response status is OK"
         assert map['response'].status == 200
+
+    }
+
+    def "a student exports questions"(){
+        given: 'a demon teacher'
+        demoStudentLogin()
+        and: 'prepare request response'
+        restClient.handler.failure = { resp, reader ->
+            [response:resp, reader:reader]
+        }
+        restClient.handler.success = { resp, reader ->
+            [response:resp, reader:reader]
+        }
+
+        when: "the web service is invoked"
+        def response = restClient.get(
+                path: "/courses/" + courseExecutionDto.getCourseExecutionId() + "/questions/export",
+                requestContentType: "application/zip"
+        )
+
+        then: "the request returns nothing"
+        response.data == null
     }
 }
