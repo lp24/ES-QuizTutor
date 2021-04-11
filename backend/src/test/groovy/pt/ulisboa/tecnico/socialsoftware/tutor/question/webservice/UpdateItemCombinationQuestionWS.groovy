52701dd32e9b38cf8ee580fbf16dab9c28f2814f
joao <joao.sanches@tecnico.ulisboa.pt>
2021-04-12 00:35:08 +0100

    [PCI 1.2] Update Question - Test web service - close #117

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/webservice/UpdateItemCombinationQuestionWS.groovy
@@ -0,0 +1,146 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question.webservice
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import groovyx.net.http.HttpResponseException
+import groovyx.net.http.RESTClient
+import org.apache.http.HttpStatus
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.tutor.auth.domain.AuthUser
+import pt.ulisboa.tecnico.socialsoftware.tutor.execution.domain.CourseExecution
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Course
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.ItemCombinationQuestionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.ItemDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.User
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UpdateItemCombinationQuestionWS extends SpockTest {
+    @LocalServerPort
+    private int port
+
+    def course
+    def courseExecution
+    def teacher
+    def response
+    def questionDto
+    def itemOneDto
+    def itemTwoDto
+
+    def setup() {
+        restClient = new RESTClient("http://localhost:" + port)
+
+        course = new Course(COURSE_1_NAME, Course.Type.EXTERNAL)
+        courseRepository.save(course)
+        courseExecution = new CourseExecution(course, COURSE_1_ACRONYM, COURSE_1_ACADEMIC_TERM, Course.Type.EXTERNAL, LOCAL_DATE_TOMORROW)
+        courseExecutionRepository.save(courseExecution)
+
+        def questionDto = new QuestionDto()
+        questionDto.setKey(1)
+        questionDto.setTitle(QUESTION_1_TITLE)
+        questionDto.setContent(QUESTION_1_CONTENT)
+        questionDto.setStatus(Question.Status.AVAILABLE.name())
+        questionDto.setQuestionDetailsDto(new ItemCombinationQuestionDto())
+
+        and: "two items"
+        def items = new ArrayList<ItemDto>()
+        def itemOneDto = new ItemDto()
+        itemOneDto.setContent(ITEM_1_CONTENT)
+        def itemTwoDto = new ItemDto()
+        itemTwoDto.setContent(ITEM_2_CONTENT)
+        items.add(itemOneDto)
+        items.add(itemTwoDto)
+        questionDto.getQuestionDetailsDto().setItems(items)
+
+    }
+
+    def "update an item combination question with two items and one connection with a teacher"() {
+        given: "an updated question"
+        def questionDto = new QuestionDto()
+        questionDto.setKey(1)
+        questionDto.setTitle(QUESTION_2_TITLE)
+        questionDto.setContent(QUESTION_2_CONTENT)
+        questionDto.setQuestionDetailsDto(new ItemCombinationQuestionDto())
+
+        and: "a teacher"
+        teacher = new User(USER_1_NAME, USER_1_EMAIL, USER_1_EMAIL,
+                User.Role.TEACHER, false, AuthUser.Type.TECNICO)
+        teacher.authUser.setPassword(passwordEncoder.encode(USER_1_PASSWORD))
+        teacher.addCourse(courseExecution)
+        courseExecution.addUser(teacher)
+        userRepository.save(teacher)
+
+        createdUserLogin(USER_1_EMAIL, USER_1_PASSWORD)
+
+        and: 'the first item and second item are changed'
+        def items = new ArrayList<ItemDto>()
+        def itemOneDto = new ItemDto()
+        itemOneDto.setContent(ITEM_2_CONTENT)
+        def itemTwoDto = new ItemDto()
+        itemTwoDto.setContent(ITEM_1_CONTENT)
+        items.add(itemOneDto)
+        items.add(itemTwoDto)
+        questionDto.getQuestionDetailsDto().setItems(items)
+
+        when: "it's created by a POST request"
+        def mapper = new ObjectMapper()
+        response = restClient.post(path: "/courses/" + course.getId() + "/questions",
+                body: mapper.writeValueAsString(questionDto), requestContentType: "application/json")
+
+        then: "check the response status"
+        response != null
+        response.status == 200
+
+        and: "if it responds with the correct questionDto"
+        def question = response.data
+        question.id != null
+        question.title == questionDto.getTitle()
+        question.content == questionDto.getContent()
+        question.status == Question.Status.AVAILABLE.name()
+    }
+
+    def "update an item combination question with with two items and one connection with a student"() {
+        given: "an updated question"
+        def questionDto = new QuestionDto()
+        questionDto.setKey(1)
+        questionDto.setTitle(QUESTION_2_TITLE)
+        questionDto.setContent(QUESTION_2_CONTENT)
+        questionDto.setQuestionDetailsDto(new ItemCombinationQuestionDto())
+
+        and: "a student"
+        teacher = new User(USER_1_NAME, USER_1_EMAIL, USER_1_EMAIL,
+                User.Role.STUDENT, false, AuthUser.Type.TECNICO)
+        teacher.authUser.setPassword(passwordEncoder.encode(USER_1_PASSWORD))
+        teacher.addCourse(courseExecution)
+        courseExecution.addUser(teacher)
+        userRepository.save(teacher)
+
+        createdUserLogin(USER_1_EMAIL, USER_1_PASSWORD)
+
+        and: 'and no changes'
+        def items = new ArrayList<ItemDto>()
+        def itemOneDto = new ItemDto()
+        itemOneDto.setContent(ITEM_2_CONTENT)
+        def itemTwoDto = new ItemDto()
+        itemTwoDto.setContent(ITEM_1_CONTENT)
+        items.add(itemOneDto)
+        items.add(itemTwoDto)
+        questionDto.getQuestionDetailsDto().setItems(items)
+
+        when: "it's created by a POST request"
+        response = restClient.post(path: "/courses/" + course.getId() + "/questions",
+                body: "error", requestContentType: "application/json")
+
+        then: "an exception is thrown"
+        def exception = thrown(HttpResponseException)
+        exception.response.status == HttpStatus.SC_BAD_REQUEST
+    }
+
+    def cleanup() {
+        userRepository.deleteById(teacher.getId())
+        courseExecutionRepository.deleteById(courseExecution.getId())
+        courseRepository.deleteById(course.getId())
+    }
+}
