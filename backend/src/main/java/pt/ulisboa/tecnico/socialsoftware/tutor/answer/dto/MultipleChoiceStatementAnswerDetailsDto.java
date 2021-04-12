7b67ba8a7fcb9523a1137bb59952ea87953621b5
BasantaPoudel <basanta.poudel@tecnico.ulisboa.pt>
2021-04-12 02:00:55 +0100

    Merge commits into PEM1.1 from pem to bring fresh codes from develop (#126)
    * Update P2.md
    
    * Merge reference (#116)
    
    * Fix error when import question from XML
    
    In Code Order Question
    Also update the corresponding test
    
    * fix cypress test due to codemirror
    
    * EditCourseDialog unit test and sonarlint fixes
    
    * unit test CoursesView.vue
    
    * integration test web service returns tar+gzip
    
    * Multiple choice question options are not removed from the database #289
    
    Added a new test case to check that options are removed when a question is updated and they aren't used.
    
    * add check result to course export integration test
    
    * upgrade frontend versions
    
    * fix update multiple choice question
    
    * move test package
    
    * Reinstalled npm and add package-lock.json.
    
    Co-authored-by: Henrique Lin <henrique.lin99@gmail.com>
    Co-authored-by: Antonio Rito Silva <rito.silva@tecnico.ulisboa.pt>
    Co-authored-by: JohnD04 <joao.g.domingos@tecnico.ulisboa.pt>
    
    * Update P2.md
    
    Co-authored-by: Lu@n@ <luana.marques@tecnico.ulisboa.pt>
    Co-authored-by: Henrique Lin <henrique.lin99@gmail.com>
    Co-authored-by: Antonio Rito Silva <rito.silva@tecnico.ulisboa.pt>
    Co-authored-by: JohnD04 <joao.g.domingos@tecnico.ulisboa.pt>

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/answer/dto/MultipleChoiceStatementAnswerDetailsDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/answer/dto/MultipleChoiceStatementAnswerDetailsDto.java
@@ -5,6 +5,7 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.MultipleChoiceQue
 
 import javax.persistence.Transient;
 
+import javax.persistence.Transient;
 
 public class MultipleChoiceStatementAnswerDetailsDto extends StatementAnswerDetailsDto {
     private Integer optionId;
