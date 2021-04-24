198533a53eb94354e7f8f7f5b9ebb4d243c1d80a
BasantaPoudel <basanta.poudel@tecnico.ulisboa.pt>
2021-04-24 21:55:46 +0100

    [PEM1.1] - Create Question - close #147

--- frontend/src/components/multiple-ordered-choice/MultipleOrderedChoiceCreate.vue
+++ frontend/src/components/multiple-ordered-choice/MultipleOrderedChoiceCreate.vue
@@ -2,18 +2,18 @@
   <div class="multiple-ordered-choice-options">
     <v-row>
       <v-col cols="1" offset="9"> Correct </v-col>
+      <v-col cols="2"> Relevance </v-col>
     </v-row>
-<!-- Option Without Relevance-->
     <v-row
       v-for="(option, index) in sQuestionDetails.options"
       :key="index"
-      data-cy="questionOptionsInput"
+      data-cy="questionOrderedOptionsInput"
     >
-      <v-col cols="10">
+      <v-col cols="9">
         <v-textarea
           v-model="option.content"
-          :label="`Option ${index + 1}`"
-          :data-cy="`Option${index + 1}`"
+          :label="`OptionWithRelevance ${index + 1}`"
+          :data-cy="`OptionWithRelevance${index + 1}`"
           rows="1"
           auto-grow
         ></v-textarea>
@@ -25,6 +25,17 @@
           :data-cy="`Switch${index + 1}`"
         />
       </v-col>
+      <v-col
+        cols="1"
+        sm="2"
+        md="1"
+      >
+        <v-text-field
+          v-model="option.relevance"
+          :data-cy="`Relevanc${index + 1}`"
+
+        ></v-text-field>
+      </v-col>
       <v-col v-if="sQuestionDetails.options.length > 2">
         <v-tooltip bottom>
           <template v-slot:activator="{ on }">
@@ -35,66 +46,13 @@
               v-on="on"
               @click="removeOption(index)"
               color="red"
-              >close</v-icon
             >
+              close</v-icon>
           </template>
           <span>Remove Option</span>
         </v-tooltip>
       </v-col>
     </v-row>
-<!-- Option Without Relevance-->
-
-<!--  OptionWith Relevance-->
-<!--    <v-row
-      v-for="(optionWithRelevance, index) in sQuestionDetails.optionsWithRelevance"
-      :key="index"
-      data-cy="questionOrderedOptionsInput"
-    >
-      <v-col cols="9">
-        <v-textarea
-          v-model="optionWithRelevance.content"
-          :label="`OptionWithRelevance ${index + 1}`"
-          :data-cy="`OptionWithRelevance${index + 1}`"
-          rows="1"
-          auto-grow
-        ></v-textarea>
-      </v-col>
-      <v-col cols="1">
-        <v-switch
-          v-model="optionWithRelevance.correct"
-          inset
-          :data-cy="`Switch${index + 1}`"
-        />
-      </v-col>
-      <v-col
-        cols="1"
-        sm="2"
-        md="1"
-      >
-        <v-text-field
-          label="Relv"
-          :data-cy="`Relv${index + 1}`"
-
-        ></v-text-field>
-      </v-col>
-      <v-col v-if="sQuestionDetails.optionsWithRelevance.length > 2">
-        <v-tooltip bottom>
-          <template v-slot:activator="{ on }">
-            <v-icon
-              :data-cy="`Delete${index + 1}`"
-              small
-              class="ma-1 action-button"
-              v-on="on"
-              @click="removeOption(index)"
-              color="red"
-            >close</v-icon
-            >
-          </template>
-          <span>Remove Option</span>
-        </v-tooltip>
-      </v-col>
-    </v-row>-->
-
     <!--  OptionWith Relevance-->
 
     <v-row>
@@ -102,16 +60,9 @@
         class="ma-auto"
         color="blue darken-1"
         @click="addOption"
-        data-cy="addOptionMultipleChoice"
+        data-cy="addOptionMultipleOrderedChoice"
         >Add Option</v-btn
       >
-<!--      <v-btn
-        class="ma-auto"
-        color="blue darken-1"
-        @click="addOption2"
-        data-cy="addOrderedOptionMultipleChoice"
-        >Add Ordered Option</v-btn
-      >-->
     </v-row>
   </div>
 </template>
@@ -129,19 +80,11 @@ export default class MultipleOrderedChoiceCreate extends Vue {
   sQuestionDetails!: MultipleOrderedChoiceQuestionDetails;
 
   addOption() {
-    this.sQuestionDetails.options.push(new Option());
+    this.sQuestionDetails.options.push(new OptionWithRelevance());
   }
-
- /* addOption2() {
-    this.sQuestionDetails.optionsWithRelevance.push(new OptionWithRelevance());
-  }*/
 
   removeOption(index: number) {
     this.sQuestionDetails.options.splice(index, 1);
   }
-
-  /*removeOption2(index: number) {
-    this.sQuestionDetails.optionsWithRelevance.splice(index, 1);
-  }*/
 }
 </script>
