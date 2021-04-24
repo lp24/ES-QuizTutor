76d992535f7656401701572081390d2404d22a8e
joaosanches2000 <joao.sanches@tecnico.ulisboa.pt>
2021-04-24 06:30:33 -0700

    new template for create PCI question

--- frontend/src/components/item-combination/ItemCombinationCreate.vue
+++ frontend/src/components/item-combination/ItemCombinationCreate.vue
@@ -14,52 +14,41 @@
         ></v-select>
       </v-col>
 
-      <v-col
-        class="d-flex"
-        cols="12"
-        sm="6"
-      >
-        <v-select
-          :items="items"
-          filled
-          label="item relacionado"
-          dense
-        ></v-select>
-      </v-col>
 
-      <v-col
-        class="d-flex"
-        cols="12"
-        sm="6"
-      >
-        <v-select
-          :items="items"
-          label="item"
-          dense
-          outlined
-        ></v-select>
-      </v-col>
+ </v-row>
+      <v-select
+            v-model="value"
+            :items="items"
+            label="items relacionados"
+            multiple
+          >
+            <template v-slot:selection="{ item, index }">
+              <v-chip v-if="index === 0">
+                <span>{{ item }}</span>
+              </v-chip>
+              <span
+                v-if="index === 1"
+                class="grey--text caption"
+              >
+                (+{{ value.length - 1 }} others)
+                </span>
+                 </template>
+                    </v-select>
+                  </v-container>
 
-      <v-col
-        class="d-flex"
-        cols="12"
-        sm="6"
-      >
-        <v-select
-          :items="items"
-          label="item relacionado"
-          dense
-          filled
-        ></v-select>
-      </v-col>
-    </v-row>
   </v-container>
 </template>
 
-<script>
-  export default {
-    data: () => ({
-      items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
-    }),
-  }
-</script>
+
+
+
+
+<<script lang="ts">
+ import { Component, Model, PropSync, Vue, Watch } from 'vue-property-decorator';
+ import ItemCombinationQuestionDetails from '@/models/management/questions/ItemCombinationQuestionDetails';
+ @Component
+ export default class ItemCombinationCreate extends Vue {
+   @PropSync('questionDetails', { type:ItemCombinationQuestionDetails })
+   sQuestionDetails!: ItemCombinationQuestionDetails;
+ }
+ </script>
