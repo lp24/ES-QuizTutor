3e4899306a99af98c965a4e651c9a055077cd307
joaosanches2000 <joao.sanches@tecnico.ulisboa.pt>
2021-05-13 12:13:25 -0700

    continuation of implementation of question answers in backend

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/answer/dto/ItemCombinationStatementAnswerDetailsDto.java
@@ -0,0 +1,66 @@
+
+package pt.ulisboa.tecnico.socialsoftware.tutor.answer.dto;
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.domain.AnswerDetails;
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.domain.ItemCombinationAnswer;
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.domain.QuestionAnswer;
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.ItemCombinationQuestion;
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.answer.domain.QuestionAnswerItem;
+
+import javax.persistence.Transient;
+
+public class ItemCombinationStatementAnswerDetailsDto extends StatementAnswerDetailsDto {
+
+    private Integer itemId;
+
+    public ItemCombinationStatementAnswerDetailsDto() {
+    }
+
+    public ItemCombinationStatementAnswerDetailsDto(ItemCombinationAnswer questionAnswer) {
+        if (questionAnswer.getItem() != null) {
+            this.itemId = questionAnswer.getItem().getId();
+        }
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    @Transient
+    private ItemCombinationAnswer createdItemCombinationAnswer;
+
+    @Override
+    public AnswerDetails getAnswerDetails(QuestionAnswer questionAnswer) {
+        createdItemCombinationAnswer = new ItemCombinationAnswer(questionAnswer);
+        questionAnswer.getQuestion().getQuestionDetails().update(this);
+        return createdItemCombinationAnswer;
+    }
+
+    @Override
+    public boolean emptyAnswer() {
+        return itemId == null;
+    }
+
+    @Override
+    public QuestionAnswerItem getQuestionAnswerItem(String username, int quizId, StatementAnswerDto statementAnswerDto) {
+        return new ItemCombinationAnswerItem(username, quizId, statementAnswerDto, this);
+    }
+
+    @Override
+    public void update(ItemCombinationQuestion question) {
+        createdItemCombinationAnswer.setItem(question, this);
+    }
+
+    @Override
+    public String toString() {
+        return "ItemCombinationStatementAnswerDto{" +
+                "itemId=" + itemId +
+                '}';
+    }
+}
