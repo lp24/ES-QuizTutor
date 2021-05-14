bf82f1c94c2cd6eabc0fe2d5e79b2751db1ddab1
BasantaPoudel <basanta.poudel@tecnico.ulisboa.pt>
2021-05-14 16:42:59 +0100

    Update pem with PEM1.8: Student Check Quiz Result #202, #203, #204 (#236)
    * PEM1.8: Student Check Quiz Result - Service Tests - Add empty test case #202
    
    * PEM1.8: Student Check Quiz Result - Webservice Tests - Test fails #203
    
    * PEM1.8: Student Check Quiz Result #202, #203, #204

--- frontend/src/views/student/quiz/ResultComponent.vue
+++ frontend/src/views/student/quiz/ResultComponent.vue
@@ -48,12 +48,14 @@ import StatementAnswer from '@/models/statement/StatementAnswer';
 import StatementCorrectAnswer from '@/models/statement/StatementCorrectAnswer';
 import Image from '@/models/management/Image';
 import MultipleChoiceAnswer from '@/components/multiple-choice/MultipleChoiceAnswer.vue';
+import MultipleOrderedChoiceAnswer from '@/components/multiple-ordered-choice/MultipleOrderedChoiceAnswer.vue';
 import CodeFillInAnswerResult from '@/components/code-fill-in/CodeFillInAnswerResult.vue';
 import CodeOrderAnswerResult from '@/components/code-order/CodeOrderAnswerResult.vue';
 
 @Component({
   components: {
     multiple_choice: MultipleChoiceAnswer,
+    multiple_ordered_choice: MultipleOrderedChoiceAnswer,
     code_fill_in: CodeFillInAnswerResult,
     code_order: CodeOrderAnswerResult,
   },
